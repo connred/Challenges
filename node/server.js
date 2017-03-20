d2e5d474d46ec0cc111948da286e13225d3d3f18
Connor Redmond <connred@icloud.com>
2017-03-20 09:08:55 -0500

    Inital Commit.

---
+++ node/server.js
@@ -0,0 +1,26 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var http = require('http');
+var socketio = require('socket.io');
+
+var app = express();
+var server = http.createServer(app);
+var io = socketio(server);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/', function(req, res) {
+    console.log('post / = ' + JSON.stringify(req.body));
+    res.status(200).send('got it');
+});
+
+app.post('/echo', function(req, res) {
+    console.log('post /echo = ' + JSON.stringify(req.body));
+    io.sockets.emit('echo', req.body);
+    res.status(201).send(req.body);
+});
+
+server.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
